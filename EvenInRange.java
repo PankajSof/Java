509e2bb980dc2e1a464697414327e8dec876a29f
Pawan Kumar <pk.soft29@gmail.com>
2020-10-14 15:45:27 +0530

    Added : Program to print the odd between two range

--- EvenInRange.java
+++ EvenInRange.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class EvenInRange {
+public class OddInRange {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter lower limit");
@@ -10,7 +10,7 @@ public class EvenInRange {
 
         for (int i = low; i <= max; i++)
         {
-            if(i%2 == 0)
+            if(i%2 != 0)
             {
                 System.out.println(i);
             }
